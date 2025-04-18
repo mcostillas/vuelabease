3f492cac79da27f0361497608da30c1e1fdb35b6
rock4rd <142190641+rock4rd@users.noreply.github.com>
2025-04-19 06:43:09 +0800

    aw

--- Frontend/src/views/BookingForm.vue
+++ Frontend/src/views/BookingForm.vue
@@ -2,7 +2,7 @@
   <div class="booking-container">
     <div class="booking-form-wrapper">
       <div class="form-header">
-        <router-link to="/login" class="back-button">
+        <router-link to="/" class="back-button">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M15 19L8 12L15 5" fill="none" stroke="#dd3859" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M8 12H20" fill="none" stroke="#dd3859" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
--- Frontend/src/views/SignupPage.vue
+++ Frontend/src/views/SignupPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="signup-container">
     <div class="signup-form-wrapper">
-      <router-link to="/login" class="back-button">
+      <router-link to="/" class="back-button">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M19 12H5" stroke="#dd3859" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
           <path d="M12 19L5 12L12 5" stroke="#dd3859" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
