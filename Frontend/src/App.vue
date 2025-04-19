977c7ec4d00c914af081f1378ed06d0f99264571
mcostillas <mcostillas_220000000711@uic.edu.ph>
2025-04-20 03:34:03 +0800

    implement a loading animation

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,53 @@
 <template>
+  <loading-animation :is-loading="isLoading" />
   <router-view />
 </template>
+
+<script>
+import LoadingAnimation from '@/components/common/LoadingAnimation.vue'
+
+export default {
+  components: {
+    LoadingAnimation
+  },
+  data() {
+    return {
+      isLoading: false,
+      loadingTimeout: null
+    }
+  },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      // Clear any existing timeout
+      if (this.loadingTimeout) {
+        clearTimeout(this.loadingTimeout)
+      }
+      
+      // Only show loading if navigation takes more than 300ms
+      this.loadingTimeout = setTimeout(() => {
+        this.isLoading = true
+      }, 300)
+      
+      next()
+    })
+    
+    this.$router.afterEach(() => {
+      // Clear the timeout to prevent showing loading if navigation was fast
+      if (this.loadingTimeout) {
+        clearTimeout(this.loadingTimeout)
+        this.loadingTimeout = null
+      }
+      
+      // If loading is already showing, hide it with a small delay for smooth transition
+      if (this.isLoading) {
+        setTimeout(() => {
+          this.isLoading = false
+        }, 200)
+      }
+    })
+  }
+}
+</script>
 
 <style>
 /* Global Styles */
